65d5327e93a3d5fedaf1483799aba1e5f013335d
Joostlek <joostlek@outlook.com>
2025-01-13 18:28:07 +0100

    Fix

--- src/demetriek/models.py
+++ src/demetriek/models.py
@@ -69,7 +69,7 @@ class Display(DataClassORJSONMixin):
 
     brightness: int
     brightness_mode: BrightnessMode
-    brightness_range: Range
+    brightness_limit: Range | None = None
     display_type: DisplayType | None = field(
         default=None, metadata=field_options(alias="type")
     )
